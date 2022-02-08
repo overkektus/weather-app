c6452f745c696c53bb72e14b13ecc8116548667c
Egor Piskunov <egor.piskunou@gmail.com>
2022-02-08 08:47:10 +0200

    feat: add test for ForecastItem

---
+++ src/components/ForecastItem.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import ForecastItem from './ForecastItem';
+
+describe('when rendered', () => {
+  it('should present max&min temp in the forecast item', () => {
+    render(<ForecastItem timestamp={1644300043} minTemp={18} maxTemp={20} weatherIconCode={'02d'} />); 
+    expect(screen.getByText(/18°C/)).toBeInTheDocument();
+    expect(screen.getByText(/20°C/)).toBeInTheDocument();
+  });
+});
